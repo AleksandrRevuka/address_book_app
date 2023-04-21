bf15fa53c17859390501dcb723ef4ae787854eab
Insight <Asmodians@hotmail.com>
2023-04-21 16:37:04 +0300

    changed class Record

--- address_book.py
+++ address_book.py
@@ -36,10 +36,9 @@ class Record:
     
     def edit_phone(self, phone_number, new_phone_number):
         """Updates an existing phone number for the contact."""
-        new_phone_number = Phone(new_phone_number)
         for phone in self.phones:
             if phone.value == phone_number:
-                phone.value = new_phone_number.value
+                phone.value = Phone(new_phone_number).value
                 break
     
     def delete_phone(self, phone_number):
